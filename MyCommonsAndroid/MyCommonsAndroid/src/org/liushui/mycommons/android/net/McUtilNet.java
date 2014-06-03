2176da09cfdb2ef998c5baede5765faa59c0532a
liushui <215266092@qq.com>
2014-06-03 20:50:53 +0800

    [+] 添加长度计算工具类

--- MyCommonsAndroid/MyCommonsAndroid/src/org/liushui/mycommons/android/net/McUtilNet.java
+++ MyCommonsAndroid/MyCommonsAndroid/src/org/liushui/mycommons/android/net/McUtilNet.java
@@ -11,10 +11,16 @@ import android.net.NetworkInfo;
  * Title: UtilNet.java<br>
  * author:xiaqiulei@gmail.com <br>
  * Date: 2012-7-31<br>
- * Version:v1.0
- * 网络工具类
+ * Version:v1.0 网络工具类
  */
 public class McUtilNet {
+
+	static NetworkInfo getInfo() {
+		Application app = McApplication.getMcAppInstance();
+		ConnectivityManager conManager = (ConnectivityManager) app.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = conManager.getActiveNetworkInfo();
+		return networkInfo;
+	}
 
 	/**
 	 * 网络是否已经连接
@@ -22,11 +28,35 @@ public class McUtilNet {
 	 * @return
 	 */
 	public static boolean isConnectInternet() {
-		Application app = McApplication.getMcAppInstance();
-		ConnectivityManager conManager = (ConnectivityManager) app.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo networkInfo = conManager.getActiveNetworkInfo();
+		NetworkInfo networkInfo = getInfo();
 		if (networkInfo != null) {
 			return networkInfo.isAvailable();
+		}
+		return false;
+	}
+
+	/**
+	 * 网络是否已经连接
+	 * 
+	 * @return
+	 */
+	public static boolean isWifiConnect() {
+		NetworkInfo networkInfo = getInfo();
+		if (networkInfo != null) {
+			return networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+		}
+		return false;
+	}
+
+	/**
+	 * 网络是否已经连接
+	 * 
+	 * @return
+	 */
+	public static boolean isMobileConnect() {
+		NetworkInfo networkInfo = getInfo();
+		if (networkInfo != null) {
+			return networkInfo.getType() == ConnectivityManager.TYPE_MOBILE;
 		}
 		return false;
 	}
