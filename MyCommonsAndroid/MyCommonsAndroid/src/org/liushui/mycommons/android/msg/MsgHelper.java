9653b851dd6349a88d2d51ed1372d3f1b3630499
liushuibufu <215266092@qq.com>
2014-06-08 16:34:15 +0800

    [*] 修改mcmsg构造

--- MyCommonsAndroid/MyCommonsAndroid/src/org/liushui/mycommons/android/msg/MsgHelper.java
+++ MyCommonsAndroid/MyCommonsAndroid/src/org/liushui/mycommons/android/msg/MsgHelper.java
@@ -6,45 +6,49 @@ import org.liushui.mycommons.android.annotation.OnMsg;
 import org.liushui.mycommons.android.log.McLog;
 import org.liushui.mycommons.android.thread.ThreadWorker;
 
-import android.os.Message;
 import android.util.SparseArray;
 
-public class MsgHelper {
+public class MsgHelper
+{
 
 	private static MsgHelper instance = new MsgHelper();
 
-	public static MsgHelper getInstance() {
+	public static MsgHelper getInstance()
+	{
 		return instance;
 	}
 
 	private SparseArray<McMsgQueuesHandle> msgQueueHandleMap;
 
-	private MsgHelper() {
+	private MsgHelper()
+	{
 		msgQueueHandleMap = new SparseArray<McMsgQueuesHandle>();
 	}
 
-	public void sendMsg(int cmd) {
-		McMsg mm = new McMsg();
-		mm.message = new Message();
-		mm.message.what = mm.msgCmd = cmd;
-		mm.time = System.currentTimeMillis();
-		sendMsg(mm);
+	public void sendMsg(int cmd)
+	{
+		sendMsg(McMsg.newInstance(cmd));
 	}
 
-	McMsgQueuesHandle getListener(int cmd) {
+	McMsgQueuesHandle getListener(int cmd)
+	{
 		McMsgQueuesHandle msgQueueHandle = msgQueueHandleMap.get(cmd);
-		if (msgQueueHandle == null) {
+		if (msgQueueHandle == null)
+		{
 			msgQueueHandle = new McMsgQueuesHandle(cmd);
 			msgQueueHandleMap.put(cmd, msgQueueHandle);
 		}
 		return msgQueueHandle;
 	}
 
-	public void sendMsg(McMsg mm) {
+	public void sendMsg(McMsg mm)
+	{
 		final McMsg temp = mm;
-		if (mm != null) {
+		if (mm != null)
+		{
 			ThreadWorker.execute(new Runnable() {
-				public void run() {
+				public void run()
+				{
 					McMsgQueuesHandle msgQueueHandle = getListener(temp.msgCmd);
 					msgQueueHandle.handleMsg(temp);
 				}
@@ -52,27 +56,36 @@ public class MsgHelper {
 		}
 	}
 
-	public void registMsg(Object obj) {
+	public void registMsg(Object obj)
+	{
 		Field[] fields = obj.getClass().getDeclaredFields();
-		if (fields != null && fields.length > 0) {
-			for (Field field : fields) {
+		if (fields != null && fields.length > 0)
+		{
+			for (Field field : fields)
+			{
 				OnMsg onMsg = field.getAnnotation(OnMsg.class);
-				if (onMsg != null) {
+				if (onMsg != null)
+				{
 
 					int[] msgs = onMsg.msg();
 
 					field.setAccessible(true);
-					try {
+					try
+					{
 						Object value = field.get(obj);
-						if (value instanceof OnMsgCallback) {
+						if (value instanceof OnMsgCallback)
+						{
 							OnMsgCallback callback = (OnMsgCallback) value;
 							registMsg(msgs, callback, onMsg);
-						} else {
+						} else
+						{
 							McLog.e(field.getName() + " in " + obj + " is not MsgCallback instance.");
 						}
-					} catch (IllegalArgumentException e) {
+					} catch (IllegalArgumentException e)
+					{
 						e.printStackTrace();
-					} catch (IllegalAccessException e) {
+					} catch (IllegalAccessException e)
+					{
 						e.printStackTrace();
 					}
 				}
@@ -80,39 +93,51 @@ public class MsgHelper {
 		}
 	}
 
-	public void registMsg(int[] cmds, OnMsgCallback callback, OnMsg onMsg) {
-		for (int cmd : cmds) {
+	public void registMsg(int[] cmds, OnMsgCallback callback, OnMsg onMsg)
+	{
+		for (int cmd : cmds)
+		{
 			McMsgQueuesHandle msgQueueHandle = getListener(cmd);
 			MsgCallbackImpl wrap = new MsgCallbackImpl(cmd, onMsg, callback);
 			msgQueueHandle.addCallback(wrap);
 
-			if (onMsg.useLastMsg()) {
+			if (onMsg.useLastMsg())
+			{
 				msgQueueHandle.doLastMsg();
 			}
 		}
 	}
 
-	public void unRegistMsg(Object obj) {
+	public void unRegistMsg(Object obj)
+	{
 		Field[] fields = obj.getClass().getDeclaredFields();
-		if (fields != null && fields.length > 0) {
-			for (Field field : fields) {
+		if (fields != null && fields.length > 0)
+		{
+			for (Field field : fields)
+			{
 				OnMsg onMsg = field.getAnnotation(OnMsg.class);
-				if (onMsg != null) {
+				if (onMsg != null)
+				{
 
 					int[] msgs = onMsg.msg();
 
 					field.setAccessible(true);
-					try {
+					try
+					{
 						Object value = field.get(obj);
-						if (value instanceof OnMsgCallback) {
+						if (value instanceof OnMsgCallback)
+						{
 							OnMsgCallback callback = (OnMsgCallback) value;
 							unRegistMsg(msgs, callback);
-						} else {
+						} else
+						{
 							McLog.e(field.getName() + " in " + obj + " is not MsgCallback instance.");
 						}
-					} catch (IllegalArgumentException e) {
+					} catch (IllegalArgumentException e)
+					{
 						e.printStackTrace();
-					} catch (IllegalAccessException e) {
+					} catch (IllegalAccessException e)
+					{
 						e.printStackTrace();
 					}
 				}
@@ -120,24 +145,30 @@ public class MsgHelper {
 		}
 	}
 
-	public void unRegistMsg(int[] cmds, OnMsgCallback callback) {
-		for (int cmd : cmds) {
+	public void unRegistMsg(int[] cmds, OnMsgCallback callback)
+	{
+		for (int cmd : cmds)
+		{
 			McMsgQueuesHandle msgQueueHandle = getListener(cmd);
 			msgQueueHandle.remove(callback);
 		}
 	}
 
-	public void clearMsgs() {
-		for (int i = 0; i < msgQueueHandleMap.size(); i++) {
+	public void clearMsgs()
+	{
+		for (int i = 0; i < msgQueueHandleMap.size(); i++)
+		{
 			int key = msgQueueHandleMap.keyAt(i);
 			McMsgQueuesHandle hanlde = msgQueueHandleMap.get(key);
-			if (hanlde != null) {
+			if (hanlde != null)
+			{
 				hanlde.clearLastMsg();
 			}
 		}
 	}
 
-	public void clearCallbacks() {
+	public void clearCallbacks()
+	{
 		msgQueueHandleMap.clear();
 	}
 }
