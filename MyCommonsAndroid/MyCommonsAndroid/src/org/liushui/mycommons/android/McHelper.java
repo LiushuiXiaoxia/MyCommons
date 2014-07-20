dce9903bda3979fe8b541708f5fcbdfecdaecbbd
xiaqiulei <xiaqiulei@126.com>
2014-07-21 00:20:04 +0800

    [+] 添加版本号

--- MyCommonsAndroid/MyCommonsAndroid/src/org/liushui/mycommons/android/McHelper.java
+++ MyCommonsAndroid/MyCommonsAndroid/src/org/liushui/mycommons/android/McHelper.java
@@ -5,11 +5,6 @@ import java.lang.annotation.Annotation;
 import org.liushui.mycommons.android.annotation.OnMsg;
 
 public class McHelper {
-	static final int version = 1;
-
-	public static int getVersion() {
-		return version;
-	}
 
 	public static OnMsg makeOnMsg(final int[] msg, final boolean ui, final boolean useLast) {
 		return new OnMsg() {
